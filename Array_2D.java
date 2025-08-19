69c25055ca238c8023604902682d15eda2248965
Ananthakrishnan K V <ananthakrishnanvenu007@gmail.com>
2025-08-19 14:42:23 +0530

    Add files via upload

---
+++ Array_2D.java
@@ -0,0 +1,24 @@
+//import java.util.Scanner;
+
+public class Array_2D {
+
+	public static void main(String[] args) {
+		//Scanner input = new Scanner(System.in);
+		
+		int [][] arr = {
+				{1,2,3},
+				{4, 5,6,7},
+				{8,9,10},
+				{11,12}
+		};
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
